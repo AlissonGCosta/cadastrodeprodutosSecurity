316eb1bac82f9cdf74edffe850f37cd0434358f1
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-20 22:35:34 -0300

    Initial commit

---
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/CadastrodeprodutosSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.costa.cadastrodeprodutosSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeprodutosSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeprodutosSecurityApplication.class, args);
+	}
+
+}
