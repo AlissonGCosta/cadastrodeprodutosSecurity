6e9e8e07a9395dfa9847cc946e3ffde95a0166a9
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-23 23:42:22 -0300

    feat(UserMapper): create a mapper to save in repository

--- src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/mapper/UserMapper.java
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/mapper/UserMapper.java
@@ -1,18 +1,31 @@
 package br.com.costa.cadastrodeprodutosSecurity.enitity.mapper;
 
+import br.com.costa.cadastrodeprodutosSecurity.enitity.UserEntity;
+
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.request.UserReqeuestDto;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.response.UserLoginResponseDto;
-import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.response.UserResponseDto;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
+
+    // criando a resposta do controller/Post
     public UserLoginResponseDto userToUserLoginResponseDto(UserReqeuestDto dto) {
         return new UserLoginResponseDto(
                 dto.email(),
                 dto.name()
 
+        );
+    }
+
+    // criando a entidade para salvar no repository
+    public UserEntity toEntity(UserReqeuestDto dto) {
+        return new UserEntity(
+                dto.name(),
+                dto.email(),
+                dto.password()
         );
     }
 }
