96ae795f79ee79cfbdce70836366969b60ff16b8
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-23 23:42:47 -0300

    feat(UserService): Aplication validation and mapper in user

--- src/main/java/br/com/costa/cadastrodeprodutosSecurity/service/UserService.java
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/service/UserService.java
@@ -5,6 +5,7 @@ import br.com.costa.cadastrodeprodutosSecurity.enitity.entityenum.EntityStatus;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.UserEntity;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.request.UserReqeuestDto;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.response.UserResponseDto;
+import br.com.costa.cadastrodeprodutosSecurity.enitity.mapper.UserMapper;
 import br.com.costa.cadastrodeprodutosSecurity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,25 +18,21 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordConfig passwordConfig;
+    private final ValidationUserService validationUserService;
+    private final UserMapper userMapper;
 
     public void createUser(UserReqeuestDto userReqeuestDto) {
 
-        if(userReqeuestDto.name().isEmpty() || userReqeuestDto.email().isEmpty() || userReqeuestDto.password().isEmpty()) {
-            throw new RuntimeException("Campos não preenchidos");
-        }
+        // criando a validação do cadastro
+        validationUserService.validateUser(userReqeuestDto);
 
 
-        UserEntity userEntity = new UserEntity();
-                userEntity.setName(userReqeuestDto.name());
-                userEntity.setEmail(userReqeuestDto.email());
-                userEntity.setPassword(passwordConfig.passwordEncoder().encode(userReqeuestDto.password()));
-                userEntity.setStatus(EntityStatus.USER);
-
-
-        userRepository.save(userEntity);
+        // salvando no MySqul
+        userRepository.save(userMapper.toEntity(userReqeuestDto));
 
     }
 
+    // metodo para listar todos os usuarios
     public List<UserResponseDto> listAllUsers() {
 
       return  userRepository.findAll().stream()
