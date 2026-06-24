4e58141b6de9e56059f524642392c85f0298b758
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-23 23:40:38 -0300

    feat(ProductService): add a new feat for validation and mapper from save, aplication solid

--- src/main/java/br/com/costa/cadastrodeprodutosSecurity/service/ProductService.java
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/service/ProductService.java
@@ -3,9 +3,13 @@ package br.com.costa.cadastrodeprodutosSecurity.service;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.ProductEntity;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.request.ProductRequestDto;
 import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.response.ProductResposneDto;
+import br.com.costa.cadastrodeprodutosSecurity.enitity.mapper.ProductMapper;
+import br.com.costa.cadastrodeprodutosSecurity.enitity.productenum.ProductEnum;
 import br.com.costa.cadastrodeprodutosSecurity.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 import java.util.List;
 
@@ -14,24 +18,16 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductValidationService productValidationService;
+    private final ProductMapper productMapper;
 
     public void createProduct(ProductRequestDto dto){
 
-        if(dto.price().intValue() < 0){
-            throw new RuntimeException("the value must be greater than zero");
-        }
+        // validando o produto
+        productValidationService.validate(dto);
 
-        if(dto.quantity() < 0){
-            throw new RuntimeException("the quantity must be greater than zero");
-        }
-
-        ProductEntity product = new ProductEntity();
-        product.setName(dto.name());
-        product.setDescription(dto.description());
-        product.setPrice(dto.price());
-        product.setQuantity(dto.quantity());
-
-        productRepository.save(product);
+        // salvando o produto
+        productRepository.save(productMapper.toProductEntity(dto));
     }
 
     public List<ProductResposneDto> findAll(){
