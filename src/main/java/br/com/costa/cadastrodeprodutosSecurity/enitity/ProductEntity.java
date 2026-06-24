dd3e0639426115f34d14104c1c0f179829732925
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-23 23:36:16 -0300

    feat(ProductEntity): Creating a constructor for Better Responsing in Service

--- src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/ProductEntity.java
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/ProductEntity.java
@@ -1,9 +1,12 @@
 package br.com.costa.cadastrodeprodutosSecurity.enitity;
 
+import br.com.costa.cadastrodeprodutosSecurity.enitity.productenum.ProductEnum;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import java.util.UUID;
 
 @Entity
@@ -30,4 +33,26 @@ public class ProductEntity {
 
     @Column(nullable = false)
     private int quantity;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private ProductEnum productEnum;
+
+    public ProductEntity(String name, String description, BigDecimal price, String category, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.createdAt = LocalDate.now();
+        this.updatedAt = LocalDate.now();
+        this.productEnum = ProductEnum.ACTIVE;
+    }
 }
