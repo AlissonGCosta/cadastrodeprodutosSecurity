ab11b8a4cc3e5458d0a0382f4a067512048c6f19
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-23 23:39:00 -0300

    feat(ProductMapper): implements a mapper from save in reposritory and minor feat in productresponselogin

--- src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/mapper/ProductMapper.java
+++ src/main/java/br/com/costa/cadastrodeprodutosSecurity/enitity/mapper/ProductMapper.java
@@ -6,14 +6,18 @@ import br.com.costa.cadastrodeprodutosSecurity.enitity.dto.response.ProductLogin
 import br.com.costa.cadastrodeprodutosSecurity.enitity.productenum.ProductEnum;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class ProductMapper {
 
+    // resposta do controler/post
     public ProductLoginResposneDto toProductLoginResponse(ProductRequestDto dto) {
         return new ProductLoginResposneDto(
                 dto.name(),
                 dto.description(),
                 dto.price(),
+                dto.category(),
                 dto.quantity()
         );
     }
